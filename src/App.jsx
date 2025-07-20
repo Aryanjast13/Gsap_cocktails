8afeee158b5568c1bb377b79ffb876cb21053de8
Aryanjast13 <aryanjastaj05@gmail.com>
2025-07-20 10:49:25 +0530

    feat: Implement initial app structure with GSAP animations and responsive navbar
    - Created main application structure in App.jsx
    - Developed Hero component with text animations using GSAP
    - Added Navbar component with scroll-triggered background color change
    - Defined navigation links and cocktail lists in constants
    - Integrated Tailwind CSS for styling and responsive design
    - Set up Vite configuration for React and Tailwind CSS
    - Added global styles in index.css

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger,SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+
+gsap.registerPlugin(ScrollTrigger,SplitText)
+
+function App() {
+
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="min-h-dvh"></div>
+     </main>
+  );
+}
+
+export default App;
